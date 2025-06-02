cad6a60fcab1637b596f0221f852e292cfd59b19
unknown <kumarthushanthy@gmail.com>
2025-06-02 14:46:25 +0530

    Added author's Thushanthy

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld { 
 public static void main(String[] args) { 
 System.out.println("Hello from GitHub and Java!"); 
+System.out.println("This code was written by Thushanthy");
 } 
 } 
